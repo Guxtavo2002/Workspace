ff304cdc0af4ed2d0548bbcd18f60a9a42a0dc62
Guxtavo2002 <gustavohenriquepj.gh@gmail.com>
2023-10-17 21:41:41 -0300

    Aula02

---
+++ JS/Aula02-Variaveis/main.js
@@ -0,0 +1,40 @@
+var nomeDoprimeiroAluno = "Eduardo"
+
+console.log(nomeDoprimeiroAluno)
+
+let numeroDeAprovados = 5
+
+console.log(numeroDeAprovados)
+
+console.clear ()
+
+let nomeAluno = "Felipe"
+
+nomeAluno = "Victor"
+
+console.log(nomeAluno)
+
+const nomeSiteWeb = "Facebook"
+
+console.log(nomeSiteWeb)
+
+// Exibir a soma de dois numeros//
+
+var numero1 = 10;
+
+var numero2 = 4;
+
+var soma = numero1 + numero2;
+
+console.log(soma)
+
+var texto1  = "conjunto das palavras escritas, em livro, folheto, documento etc."
+var texto2  = " (p.opos. a comentários, aditamentos, sumário etc.); redação original de qualquer obra escrita."
+
+var concatenar = texto1 + texto2 
+
+console.log(concatenar)
+
+console.clear()
+
+var osDeVerdadeAGenteSabeQuemE = false;
