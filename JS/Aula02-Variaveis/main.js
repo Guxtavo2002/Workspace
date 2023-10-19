6e564eb6cd0d784f119296defa808fda6d712ae6
Guxtavo2002 <gustavohenriquepj.gh@gmail.com>
2023-10-18 21:40:24 -0300

    aula02

--- JS/Aula02-Variaveis/main.js
+++ JS/Aula02-Variaveis/main.js
@@ -46,3 +46,30 @@ console.log( "5" - 1)
 console.log( 5 + "1")
 
 console.log( "five" * 2)
+
+console.clear()
+
+//incremento = incremento + 6 //
+
+// incremento += 6
+
+// console.log(incremento)
+
+console.clear()
+
+var imparPar = 3%2
+
+console.log("Se o resultado for 1 então o número é impar se o resultado for 0 é par, esse é o resultado: " + imparPar)
+
+console.clear()
+
+var comparacao = 2 > 3
+
+console.log("2 é menor que 3?" + comparacao) 
+
+console.clear()
+
+var doisSinaisDeIgual = 2 === 3
+
+console.log(doisSinaisDeIgual)
+
