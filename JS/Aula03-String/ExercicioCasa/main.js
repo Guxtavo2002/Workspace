fb818db9461f87821d33905a0cf99cb1d55ad6dd
Guxtavo2002 <143458783+Guxtavo2002@users.noreply.github.com>
2023-10-20 20:09:24 -0300

    Update main.js

--- JS/Aula03-String/ExercicioCasa/main.js
+++ JS/Aula03-String/ExercicioCasa/main.js
@@ -3,5 +3,4 @@ var texto  =  "Estamos aprendendo JavaScript e seremos futuros Devs"
 console.log("caracter que está na posição 19 da string: " + texto.chartArt(19))
 console.log("Quantidade de caracteres da string: " + texto.length)
 console.log("String em maiúsculo: " + texto.toUpperCase())
-console.log("Apenas a palavra Devs da string: " + texto)
-console.log(sub.substring(48))
+console.log("Apenas a palavra Devs da string: " + texto.substring(48))
