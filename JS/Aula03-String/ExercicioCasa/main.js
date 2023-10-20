c603a586ecab4cee3c8b65dc3741a8de7c9c0220
Guxxtavo2002 <gustavohenriquepj.gh@gmail.com>
2023-10-20 20:30:16 -0300

    attExercicio3

--- JS/Aula03-String/ExercicioCasa/main.js
+++ JS/Aula03-String/ExercicioCasa/main.js
@@ -1,6 +1,7 @@
 var texto  =  "Estamos aprendendo JavaScript e seremos futuros Devs"
 
-console.log("caracter que está na posição 19 da string: " + texto.chartArt(19))
+console.log(texto)
+console.log("caracter que está na posição 19 da string: " + texto.charAt(19))
 console.log("Quantidade de caracteres da string: " + texto.length)
 console.log("String em maiúsculo: " + texto.toUpperCase())
 console.log("Apenas a palavra Devs da string: " + texto.substring(48))
