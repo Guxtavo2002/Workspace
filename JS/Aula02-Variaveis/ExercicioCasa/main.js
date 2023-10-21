875f37f639540e75eb392fcfc11570af0c0c5c7e
Guxxtavo2002 <gustavohenriquepj.gh@gmail.com>
2023-10-20 21:23:07 -0300

    attExercicioAula02

--- JS/Aula02-Variaveis/ExercicioCasa/main.js
+++ JS/Aula02-Variaveis/ExercicioCasa/main.js
@@ -3,6 +3,6 @@ var numero2 = 9;
 var numero3 = 7;
 var numero4 = 10;
 
-var media = numero1 + numero2 + numero3 + numero4;
+var media = numero1 + numero2 + numero3 + numero4 / 4 ;
 
 console.log("A média do aluno com as 4 notas é: " + media)
