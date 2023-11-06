f3d8f3acf91e6b15a32b3b00c0f91c891ef0c913
Guxtavo2002 <gustavohenriquepj.gh@gmail.com>
2023-11-06 19:18:11 -0300

    attPcCurso

---
+++ JS/Aula08-Objetos/main.js
@@ -0,0 +1,24 @@
+const celular = {
+    marca: 'Apple',
+    modelo: 'Iphone 15 pro Max',
+    cor: 'Rosa',
+    memoria: '512gb',
+    memoriaRam: '16gb'
+}
+
+console.log(celular)
+
+//Eventos
+console.clear();
+const boasVindas = () => {
+alert('Bem vindo a nossa página');
+console.log('Bem vindo a nossa página');
+};
+const eventClique = () => {
+console.log('Você clicou no botão');
+};
+
+const mouseEmCima = () => {
+    console.log('Mouse está em cima do título');
+};
+
