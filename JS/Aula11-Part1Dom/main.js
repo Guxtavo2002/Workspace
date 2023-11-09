4eb54cbde242c39189f23851f1a3a16eb5f37518
Guxtavo2002 <gustavohenriquepj.gh@gmail.com>
2023-11-08 21:05:22 -0300

    aula11

---
+++ JS/Aula11-Part1Dom/main.js
@@ -0,0 +1,9 @@
+//criação da variavel e criação do button 
+let botao = document.createElement('button');
+//adicionar o filho(botao) na body
+document.body.appendChild(botao)
+//adiconar um texto no botão
+botao.innerHTML = 'Acessa aí'
+// adicionar estilo ao botão
+botao.style.border = 'solid 2px red'
+ 
