12026d3ec94012690bc3efad93ebe3e29c2c9aa0
Guxxtavo2002 <gustavohenriquepj.gh@gmail.com>
2023-11-15 14:17:21 -0300

    aula07ExercicioCasa

---
+++ JS/Aula08-Objetos/ExercicioCasa/main.js
@@ -0,0 +1,24 @@
+function alertCookie(){
+    console.log('Você quer um cookie')
+}
+
+const alertSucesso = () => {
+    console.log('Mensagem gerada com sucesso')
+}
+
+const notebook = {
+    marca: 'Asus',
+    memoriaRam: '8gb',
+    memoria: '256gb',
+    cor: 'cinza',
+    Modelo: 'M515DA-BR1213W',
+}
+
+console.log(notebook)
+
+function multiplos (valor1, valor2) {
+    return valor1 * valor2;
+}
+
+let resultado = multiplos (4, 365)
+console.log(resultado)
