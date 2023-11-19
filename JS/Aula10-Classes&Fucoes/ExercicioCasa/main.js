deddbc8817840b1b848f12db729b030fa1110147
Guxxtavo2002 <gustavohenriquepj.gh@gmail.com>
2023-11-19 20:16:34 -0300

    aula10Exerciciocasa

--- JS/Aula10-Classes&Fucoes/ExercicioCasa/main.js
+++ JS/Aula10-Classes&Fucoes/ExercicioCasa/main.js
@@ -11,19 +11,27 @@ class contaBancaria {
     }
 
     contaPoupança() {
-        return this.salario - 1.5
+        const aplicaDesconto = (this.salario * 1.5)/100
+        const resultadododesconto = this.salario - aplicaDesconto
+        return `O salário novo é: ${resultadododesconto}`
     }
 
     contaCorrente() {
-        return this.salario - 3.6
+        const aplicaDesconto = (this.salario * 3.6)/100
+        const resultadoaplicado = this.salario -aplicaDesconto;
+        return `O salário novo é: ${resultadoaplicado}`
     }
 
     contaEstudante() {
-        return this.salario - 1.2
+        const aplicaDesconto = (this.salario * 1.2)/100
+        const resultadoaplicado = this.salario - aplicaDesconto
+        return `O salário novo é: ${resultadoaplicado}`
     }
 }
 
-var aluno = new contaBancaria('Gustavo', '21', 2100, 'Masculino', '165', '74589', 'contaCoorente')
+var aluno1 = new contaBancaria('Gustavo', '21', 2100, 'Masculino', '165', '74589', 'contaEstudante')
+var aluno2 = new contaBancaria('Marcia', '50', 1800, 'Feminino', '670', '458615', 'contaPoupança');
+var aluno3 = new contaBancaria('Silvio', '52', 3000, 'Masculino', '560', '645687', 'contaCorrente');
 
-console.log(aluno)
-console.log(aluno.contaEstudante())
+console.log(aluno1)
+console.log(aluno1.contaEstudante())
